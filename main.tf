3d67a698c9f01bb99a3d94e9ec5bb0d497a22c81
Tsvetomir Iliev <93545848+cs-iliev@users.noreply.github.com>
2025-09-03 13:16:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "firewall_policy" {
 }
 
 module "virtual_wan" {
-  source  = "https://github.com/cs-iliev/terraform-azurerm-avm-ptn-virtualwan.git"
+  source  = "git::https://github.com/cs-iliev/terraform-azurerm-avm-ptn-virtualwan.git"
 
   location                              = var.virtual_wan_settings.location
   resource_group_name                   = var.virtual_wan_settings.resource_group_name
